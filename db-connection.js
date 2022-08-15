cc8d44cf54f1e9a0d6d9713eee3c2a6fa974907e
ethernetwins <111263766+ethernetwins@users.noreply.github.com>
2022-08-15 17:52:11 +0800

    Update db-connection.js

--- db-connection.js
+++ db-connection.js
@@ -1,15 +1,15 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'foodsq.cwmvcgt1jgye.us-east-1.rds.amazonaws.com',
     port: '3306',
-    user: 'root',
-    password: 'root',
+    user: 'admin',
+    password: 'Coughing1',
     database: 'movie_info'
 });
 
 connection.connect(err => {  // test out connetion and console.log error if there is one
     if (err) throw err;
-    console.log('Connected To DB');
+    console.log('Connected To AWS DB');
 }); 
 module.exports = connection;
 
